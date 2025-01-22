364100ee67b58ad0d434c25b1db0f9db825a92cf
bhagath555 <mbhagath555@gmail.com>
2025-01-22 17:11:45 +0100

    Initial code run with opencv library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main()
 {
     std::cout << "Testing the template\n";
+
+    cv::Mat image = cv::imread("../docs/cameraman.png");
+
+    if (image.empty()){
+        std::cerr << "Error : could not open the image" << std::endl;
+    }
+
+    cv::imshow("Cameraman image", image);
+    cv::waitKey(0);
     return 0;
 }
